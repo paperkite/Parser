637a8b77b75cc289e1a313da964e8acdfbd533f3
Walig <c.walig@gmail.com>
2016-06-02 11:54:07 +1200

    Initial Commit

---
+++ Parser/Parser.h
@@ -0,0 +1,19 @@
+//
+//  Parser.h
+//  Parser
+//
+//  Created by Walig Castain on 2/06/16.
+//  Copyright © 2016 PaperKite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Parser.
+FOUNDATION_EXPORT double ParserVersionNumber;
+
+//! Project version string for Parser.
+FOUNDATION_EXPORT const unsigned char ParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Parser/PublicHeader.h>
+
+
