b1d417950cd144288c390aa8c74c7d52757c723c
Walig <c.walig@gmail.com>
2016-07-14 09:10:59 +1200

    Add fetchOptionalArray to the Parser

--- ParserTests/ParserTests.swift
+++ ParserTests/ParserTests.swift
@@ -9,15 +9,15 @@
 import XCTest
 @testable import Parser
 
-enum Type: String {
-    case Female = "female"
-    case Male = "male"
+enum ContractType: String {
+    case Employee = "employee"
+    case Contractor = "contractor"
 }
 
 class Employee: 🔨 {
     
     let name: String
-    let type: Type
+    let contractType: ContractType
 
     required init?(jsonDictionary: [String: AnyObject]) {
         
@@ -25,7 +25,7 @@ class Employee: 🔨 {
 
         do {
             self.name = try parser.fetch("name")
-            self.type = try parser.fetch("type") {  Type(rawValue: $0) }
+            self.contractType = try parser.fetch("type") {  ContractType(rawValue: $0) }
             
         } catch let error {
             print(error)
@@ -45,7 +45,9 @@ class Company: 🔨 {
     let numberOfEmployee: Int
     let averageAgeEmployee: Int?
     let team: [Employee]
-    
+    let optionalTeam: [Employee]?
+    let optionalTeamNested: [Employee]?
+
     required init?(jsonDictionary: [String: AnyObject]) {
         
         let parser = Parser(dictionary: jsonDictionary)
@@ -59,6 +61,8 @@ class Company: 🔨 {
             self.numberOfEmployee = try parser.fetch(["team","number_employee"])
             self.averageAgeEmployee = try parser.fetchOptional(["team", "average_age"])
             self.team = try parser.fetchArray(["team","list"]) { Employee(jsonDictionary: $0) }
+            self.optionalTeam = try parser.fetchOptionalArray("optional_team") { Employee(jsonDictionary: $0) }
+            self.optionalTeamNested = try parser.fetchOptionalArray(["optional_team_nested", "list"]) { Employee(jsonDictionary: $0) }
 
         } catch let error {
             print(error)
@@ -69,31 +73,58 @@ class Company: 🔨 {
 }
 
 class ParserTests: XCTestCase {
-    
-    let validJsonDictionary: [String: AnyObject] = [
-        "id": "2c16035c-9a28-42fc-86c0-e517c70798a3",
-        "name": "PaperKite",
-        "age": 8,
-        "coding_enabled": true,
-        "team": [
-            "number_employee": 2,
-            "average_age": 28,
-            "list": [
+
+    /**
+     Should be all parsed correctly
+     */
+    func testInitializer() {
+        
+        let validJsonDictionary: [String: AnyObject] = [
+            "id": "2c16035c-9a28-42fc-86c0-e517c70798a3",
+            "name": "PaperKite",
+            "age": 8,
+            "coding_enabled": true,
+            "team": [
+                "number_employee": 2,
+                "average_age": 28,
+                "list": [
+                    [
+                        "name": "paul",
+                        "type": "contractor"
+                    ],
+                    [
+                        "name": "tom",
+                        "type": "employee"
+                    ]
+                ]
+            ],
+            "optional_team": [
                 [
-                    "name": "paul",
-                    "type": "male"
+                    "name": "rebecca",
+                    "type": "employee"
                 ],
                 [
-                    "name": "tom",
-                    "type": "female"
+                    "name": "nate",
+                    "type": "contractor"
+                ]
+            ],
+            "optional_team_nested": [
+                "number_employee": 2,
+                "average_age": 35,
+                "list": [
+                    [
+                        "name": "patrick",
+                        "type": "contractor"
+                    ],
+                    [
+                        "name": "andrew",
+                        "type": "employee"
+                    ]
                 ]
             ]
         ]
-    ]
-    
-    func testInitializer() {
         
-        if let company = Company(jsonDictionary: self.validJsonDictionary) {
+        if let company = Company(jsonDictionary: validJsonDictionary) {
             XCTAssert(company.id == "2c16035c-9a28-42fc-86c0-e517c70798a3")
             XCTAssert(company.name == "PaperKite")
             XCTAssert(company.age == 8)
@@ -103,17 +134,229 @@ class ParserTests: XCTestCase {
 
             if let employeeOne: Employee = company.team[0], let employeeTwo: Employee = company.team[1] {
                 XCTAssert(employeeOne.name == "paul")
-                XCTAssert(employeeOne.type == .Male)
+                XCTAssert(employeeOne.contractType == .Contractor)
 
                 XCTAssert(employeeTwo.name == "tom")
-                XCTAssert(employeeTwo.type == .Female)
+                XCTAssert(employeeTwo.contractType == .Employee)
             } else {
                 XCTFail("Unable to assert employees")
             }
             
+            if let employeeOne: Employee = company.optionalTeam![0], let employeeTwo: Employee = company.optionalTeam![1] {
+                XCTAssert(employeeOne.name == "rebecca")
+                XCTAssert(employeeOne.contractType == .Employee)
+                
+                XCTAssert(employeeTwo.name == "nate")
+                XCTAssert(employeeTwo.contractType == .Contractor)
+            } else {
+                XCTFail("Unable to assert optional_team")
+            }
+            
+            if let employeeOne: Employee = company.optionalTeamNested![0], let employeeTwo: Employee = company.optionalTeamNested![1] {
+                XCTAssert(employeeOne.name == "patrick")
+                XCTAssert(employeeOne.contractType == .Contractor)
+                
+                XCTAssert(employeeTwo.name == "andrew")
+                XCTAssert(employeeTwo.contractType == .Employee)
+            } else {
+                XCTFail("Unable to assert optional_nested_team")
+            }
+
         } else {
             XCTFail("fail to init Company")
         }
     }
     
+    /**
+     The age in the JSON is not a number but a string
+     */
+    func testWrongAgeTypeInJSON() {
+        
+        // age should be a Int not a string
+        let invalidJsonDictionary: [String: AnyObject] = [
+            "id": "2c16035c-9a28-42fc-86c0-e517c70798a3",
+            "name": "PaperKite",
+            "age": "8", // <---- HERE
+            "coding_enabled": true,
+            "team": [
+                "number_employee": 2,
+                "average_age": 28,
+                "list": [
+                    [
+                        "name": "paul",
+                        "type": "male"
+                    ],
+                    [
+                        "name": "tom",
+                        "type": "female"
+                    ]
+                ]
+            ]
+        ]
+
+        let company = Company(jsonDictionary: invalidJsonDictionary)
+        XCTAssertNil(company, "Company should be nil, as age is a string in the JSON")
+    }
+    
+    /**
+     The coding_enabled in the JSON is not a number but a string
+     */
+    func testWrongBoolTypeInJSON() {
+        
+        // coding_enabled should be a Bool not a string
+        let invalidJsonDictionary: [String: AnyObject] = [
+            "id": "2c16035c-9a28-42fc-86c0-e517c70798a3",
+            "name": "PaperKite",
+            "age": 8,
+            "coding_enabled": "true", // <---- HERE
+            "team": [
+                "number_employee": 2,
+                "average_age": 28,
+                "list": [
+                    [
+                        "name": "paul",
+                        "type": "male"
+                    ],
+                    [
+                        "name": "tom",
+                        "type": "female"
+                    ]
+                ]
+            ]
+        ]
+        
+        let company = Company(jsonDictionary: invalidJsonDictionary)
+        XCTAssertNil(company, "Company should be nil, as coding_enabled is a string in the JSON")
+    }
+    
+    /**
+     Test optional employee array
+     */
+    func testOptionalEmployeeArrayBeingNilInJSON() {
+        
+        // no optionalTeam in the JSON
+        let validJsonDictionary: [String: AnyObject] = [
+            "id": "2c16035c-9a28-42fc-86c0-e517c70798a3",
+            "name": "PaperKite",
+            "age": 8,
+            "coding_enabled": true,
+            "team": [
+                "number_employee": 2,
+                "average_age": 28,
+                "list": [
+                    [
+                        "name": "paul",
+                        "type": "employee"
+                    ],
+                    [
+                        "name": "tom",
+                        "type": "contractor"
+                    ]
+                ]
+            ]
+        ]
+        
+        if let company = Company(jsonDictionary: validJsonDictionary) {
+            XCTAssert(company.id == "2c16035c-9a28-42fc-86c0-e517c70798a3")
+            XCTAssert(company.name == "PaperKite")
+            XCTAssert(company.age == 8)
+            XCTAssert(company.codingEnabled == true)
+            XCTAssert(company.numberOfEmployee == 2)
+            XCTAssert(company.averageAgeEmployee == 28)
+
+            if let employeeOne: Employee = company.team[0], let employeeTwo: Employee = company.team[1] {
+                XCTAssert(employeeOne.name == "paul")
+                XCTAssert(employeeOne.contractType == .Employee)
+                
+                XCTAssert(employeeTwo.name == "tom")
+                XCTAssert(employeeTwo.contractType == .Contractor)
+            } else {
+                XCTFail("Unable to assert employees")
+            }
+            
+            XCTAssertNil(company.optionalTeam)
+            XCTAssertNil(company.optionalTeamNested)
+
+        } else {
+            XCTFail("fail to init Company")
+        }
+    }
+    
+    /**
+     Test wrong enum type
+     */
+    func testOptionalEmployeeArrayBeingNilWithWrongEnumInJSON() {
+        
+        // contactor type is wrong
+        let validJsonDictionary: [String: AnyObject] = [
+            "id": "2c16035c-9a28-42fc-86c0-e517c70798a3",
+            "name": "PaperKite",
+            "age": 8,
+            "coding_enabled": true,
+            "team": [
+                "number_employee": 2,
+                "average_age": 28,
+                "list": [
+                    [
+                        "name": "paul",
+                        "type": "employee"
+                    ],
+                    [
+                        "name": "tom",
+                        "type": "contractor"
+                    ]
+                ]
+            ],
+            "optional_team": [
+                [
+                    "name": "paul",
+                    "type": "werwrwr" // <- wrong value
+                ],
+                [
+                    "name": "tom",
+                    "type": "sfpsdpfosdf" // <- wrong value
+                ]
+            ],
+            "optional_team_nested": [
+                "number_employee": 2,
+                "average_age": 28,
+                "list": [
+                    [
+                        "name": "paul",
+                        "type": "werwrwr" // <- wrong value
+                    ],
+                    [
+                        "name": "tom",
+                        "type": "sfpsdpfosdf" // <- wrong value
+                    ]
+                ]
+            ]
+        ]
+        
+        if let company = Company(jsonDictionary: validJsonDictionary) {
+            XCTAssert(company.id == "2c16035c-9a28-42fc-86c0-e517c70798a3")
+            XCTAssert(company.name == "PaperKite")
+            XCTAssert(company.age == 8)
+            XCTAssert(company.codingEnabled == true)
+            XCTAssert(company.numberOfEmployee == 2)
+            XCTAssert(company.averageAgeEmployee == 28)
+            
+            if let employeeOne: Employee = company.team[0], let employeeTwo: Employee = company.team[1] {
+                XCTAssert(employeeOne.name == "paul")
+                XCTAssert(employeeOne.contractType == .Employee)
+                
+                XCTAssert(employeeTwo.name == "tom")
+                XCTAssert(employeeTwo.contractType == .Contractor)
+            } else {
+                XCTFail("Unable to assert employees")
+            }
+
+            XCTAssertNil(company.optionalTeam)
+            XCTAssertNil(company.optionalTeamNested)
+            
+        } else {
+            XCTFail("fail to init Company")
+        }
+    }
+
 }
